871bd142afec86f335d5eeca90eb34f0a5c2df0f
igormuba <igorm-uba@hotmail.com>
2018-12-18 15:14:07 -0200

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -1,26 +1,40 @@
 let query = {
     tag: 'igormuba',
-    limit: 3
+    limit: 4
 }
 
-steem.api.getDiscussionsByFeed(query, function (err, result) {
-    console.log(err, result);
-    for (let i=0; i<result.length; i++) {
-        let iToString = (i+1).toString();
-        let converter = new showdown.Converter()
-        let text = result[i].body
-        let html = converter.makeHtml(text);
-        document.getElementById(iToString).innerHTML = `
-        <h1>`+result[i].title+`</h1>
-        <h2>`+result[i].author+`</h2>
-        <p>`+html+`</p>
-        <hr>
-        `
-    }
-  });
+let posts;
 
-function generatePost(post){
-      let author = post.author;
-      let title = post.title;
-      let body = post.body;
-  }
+
+
+function getTheFeed(){
+    steem.api.getDiscussionsByFeed(query, function (err, result) {
+        console.log(err, result);
+        posts=result;
+        for (let i=0; i<3; i++) {
+            let iToString = (i+1).toString();
+            let converter = new showdown.Converter()
+            let text = result[i].body
+            let html = converter.makeHtml(text);
+            document.getElementById(iToString).innerHTML = `
+            <h1>`+result[i].title+`</h1>
+            <h2>`+result[i].author+`</h2>
+            <p>`+html+`</p>
+            <hr>
+            `
+        }
+    });
+}
+
+getTheFeed();
+
+function nextPage(){
+    query = {
+        tag: 'igormuba',
+        limit: 4,
+        start_author: posts[3].author,
+        start_permlink: posts[3].permlink
+    }
+
+    getTheFeed();
+}
