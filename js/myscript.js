8fa244d95675bf488b4e7b9faf99cb617f398a66
igormuba <igorm-uba@hotmail.com>
2018-12-20 17:57:20 -0200

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -1,37 +1,41 @@
 let query = {
     tag: 'igormuba',
-    limit: 3
+    limit: 4
 }
 
 let posts;
 
-getTheFeed();
+
 
 function getTheFeed(){
     steem.api.getDiscussionsByFeed(query, function (err, result) {
         console.log(err, result);
         posts=result;
-        for (let i=0; i<result.length; i++) {
+        for (let i=0; i<3; i++) {
             let iToString = (i+1).toString();
             let converter = new showdown.Converter()
             let text = result[i].body
             let html = converter.makeHtml(text);
             document.getElementById(iToString).innerHTML = `
+            <hr class="horizontal-ruler">
             <h1>`+result[i].title+`</h1>
-            <h2>`+result[i].author+`</h2>
+            <h2 class="post-author"><em>by: @`+result[i].author+`</em></h2>
+            <hr class="horizontal-ruler">
             <p>`+html+`</p>
-            <hr>
+            <hr class="horizontal-ruler">
             `
         }
     });
 }
 
+getTheFeed();
+
 function nextPage(){
     query = {
         tag: 'igormuba',
-        limit: 3,
-        start_author: posts[2].author,
-        start_permlink: posts[2].permlink
+        limit: 4,
+        start_author: posts[3].author,
+        start_permlink: posts[3].permlink
     }
 
     getTheFeed();
